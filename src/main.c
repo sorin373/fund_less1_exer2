2123ba787c2efaf634d4fbfdb3a2dc6109368887
Sorin Tudose <sorin.andrei.tudose@gmail.com>
2024-09-25 22:48:14 +0300

    Update

--- src/main.c
+++ src/main.c
@@ -8,36 +8,63 @@
 #include <zephyr/drivers/gpio.h>
 
 /* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   1000
+#define SLEEP_TIME_MS   2000
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
+#define LED1_NODE DT_ALIAS(led1)
+#define LED2_NODE DT_ALIAS(led2)
+#define LED3_NODE DT_ALIAS(led3)
 
 /*
  * A build error on this line means your board is unsupported.
  * See the sample documentation for information on how to fix this.
  */
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
+static const struct gpio_dt_spec led2 = GPIO_DT_SPEC_GET(LED2_NODE, gpios);
+static const struct gpio_dt_spec led3 = GPIO_DT_SPEC_GET(LED3_NODE, gpios);
+
+struct gpio_dt_spec v[4];
 
 int main(void)
 {
+	const struct device *dev;
+	dev = DEVICE_DT_GET(DT_NODELABEL(uart0));
+
+	if (!device_is_ready(dev))
+		return 0;
+
 	int ret;
 
-	if (!gpio_is_ready_dt(&led)) {
-		return 0;
-	}
+	v[0] = led;
+	v[1] = led1;
+	v[2] = led2;
+	v[3] = led3;
 
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0) {
-		return 0;
-	}
-
-	while (1) {
-		ret = gpio_pin_toggle_dt(&led);
-		if (ret < 0) {
+	for (unsigned int i = 0; i < 4; ++i)
+	{
+		if (!gpio_is_ready_dt(&v[i]))
 			return 0;
+
+		ret = gpio_pin_configure_dt(&v[i], GPIO_OUTPUT_ACTIVE);
+
+		if (ret < 0) 
+			return 0;
+	}
+
+	while (1) 
+	{
+		for (unsigned int i = 0; i < 4; ++i)
+		{
+			ret = gpio_pin_toggle_dt(&v[i]);
+
+			if (ret < 0)
+				return 0;
 		}
+	
 		k_msleep(SLEEP_TIME_MS);
 	}
+
 	return 0;
 }
